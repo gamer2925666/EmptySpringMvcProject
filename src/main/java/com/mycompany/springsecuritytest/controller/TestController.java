24fd465531081a6f6753727329480316319ed9ac
Ben <gamer2925666@gmail.com>
2019-04-18 16:17:01 +0800

    新增測試setenv的測試API

--- src/main/java/com/mycompany/springsecuritytest/controller/TestController.java
+++ src/main/java/com/mycompany/springsecuritytest/controller/TestController.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,13 +35,23 @@ public class TestController {
 
                 return modelAndView;
         }
-        
+
+//        @Value("#{testValue}")
+//        String value;
+
         @GetMapping(value="/cat/{id}")
         @ResponseBody
         public List<Cat> test(@PathVariable Long id){
                 List<Cat> list = new ArrayList<>();
-                Cat cat = new Cat(id, "testCat", "blue", 16);
+                Cat cat = new Cat(id, "testCat", "grey", 16);
                 list.add(cat);
                 return list;
+        }
+
+        @GetMapping(value = "/getTestValue")
+        @ResponseBody
+        public String test1(){
+                //取得tomcat中setenv.bat中設定的變數
+                return System.getenv("testCat");
         }
 }
